51fbedf5fc00a3163aa1a684d1d9ba47e98eaee5
Leya Joy <82429107+leyajoy@users.noreply.github.com>
2024-04-11 17:21:07 +0530

    unit testing

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,9 +1,19 @@
 import MyClass from "../src/myClass.js";
-var myObj = new MyClass();
-import { expect as _expect } from "chai";
-var expect = _expect;
+import { expect } from "chai";
+import sinon from "sinon";
+
+const myObj = new MyClass();
+
 describe("Test suit", function () {
 	it("Test the add method", function () {
 		expect(myObj.add(1, 2)).to.be.equal(3);
+	});
+
+	it("spy the add method", function () {
+		const spy = sinon.spy(myObj, "add");
+		myObj.callAnotherFn(10, 20);
+		// sinon.assert.calledOnce(spy);
+		expect(spy.calledOnce).to.be.true;
+		expect(spy.calledWith(10, 20)).to.be.true;
 	});
 });
--- src/myClass.js
+++ src/myClass.js
@@ -3,8 +3,11 @@ class MyClass {
 		console.log("initiate");
 	}
 	add(arg1, arg2) {
-		var result;
-		result = arg1 + arg2;
+		var result = arg1 + arg2;
+		return result;
+	}
+	callAnotherFn(arg1, arg2) {
+		var result = this.add(arg1, arg2);
 		return result;
 	}
 }
